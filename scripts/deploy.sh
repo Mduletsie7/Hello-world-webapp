d4558c22a9700decf49edcf178be4e1cb324e4fe
Mdumisi Kelvin Letsie <mdumisi@kineticskunk.com>
2024-04-16 14:17:50 +0200

    Debug fixe deploy stage

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
-# Set environment variables
-AZURE_WEBAPP_NAME="php-demo-webapp"    # set this to your application's name
-AZURE_WEBAPP_PACKAGE_PATH="./php"      # set this to the path to your web app project, defaults to the repository root
-PHP_VERSION="8.x"                  # set this to the PHP version to use
-
 # Set permissions
 sudo chmod -R 400 $GITHUB_WORKSPACE/*
 
 # Define the deploy function
 deploy() {
-    # Checkout the repository
-    git checkout $GITHUB_SHA
-
-    # Download artifact from build job
-    echo "Downloading artifact from build job..."
-    gh run download -n php-app
-
-    # Login to Azure
-    echo "Logging in to Azure..."
-    az login --service-principal -u $AZURE_SP_APP_ID -p $AZURE_SP_PASSWORD --tenant $AZURE_TENANT_ID
-
     # Deploy to Azure Web App
     echo "Deploying to Azure Web App..."
     az webapp deploy --name $AZURE_WEBAPP_NAME --package $AZURE_WEBAPP_PACKAGE_PATH --output none
